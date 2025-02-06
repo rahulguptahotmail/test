2e98065f9373b250ec398330c3226426ebe7a7d3
Rahul Gupta <rahulguptahotmail@gmail.com>
2025-02-06 19:20:17 +0530

    fourth

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import os from 'os'
+import path from 'path'
 import express from 'express'
 
 
 const app = express()
+
+app.get('/',(req,res)=>{
+    res.end(path.resolve())
+})
 
 app.get('/home',(req,res)=>{
     res.end(os.homedir())
